8a1fab72b80edc1462bb2068ada8d917628815b9
cy_ryo_hashimoto <cy_ryo_hashimoto@cybird.co.jp>
2015-11-16 18:51:08 +0900

    Buttonクラス実装

---
+++ UI/Button.cpp
@@ -0,0 +1,186 @@
+//
+//  Button.cpp
+//  HRStandardLibrary
+//
+//  Created by c1790 on 2015/11/16.
+//
+//
+
+#include "Button.h"
+#include "HRLog.h"
+#include "NodeUtility.h"
+
+
+using namespace HR;
+USING_NS_CC;
+
+
+#pragma mark - Constructor & Destructor
+
+Button::Button()
+: _normalTexture(nullptr)
+, _touchedTexture(nullptr)
+, _isEnableScaleEffect(true)
+, _isEnableBrightnessEffect(true)
+{
+}
+
+Button::~Button()
+{
+}
+
+
+
+#pragma mark - Create & Initialize Methods
+
+Button *Button::create()
+{
+    Button *pRet = new (std::nothrow) Button();
+    if (pRet && pRet->init()) {
+        pRet->autorelease();
+        return pRet;
+    }
+    
+    CC_SAFE_DELETE(pRet);
+    return nullptr;
+}
+
+bool Button::init()
+{
+    if (!this->TouchableSprite::init()) return false;
+    
+    return true;
+}
+
+Button *Button::createWithFiles(const std::string &normalFile,
+                                const std::string &touchedFile,
+                                bool scaleEffect      /* = true */,
+                                bool brightnessEffect /* = true */,
+                                bool isEnable         /* = true */)
+{
+    Button *pRet = Button::create();
+    if (pRet->initWithFiles(normalFile, touchedFile, scaleEffect, brightnessEffect, isEnable))
+        return pRet;
+    /* else */
+        return nullptr;
+}
+
+bool Button::initWithFiles(const std::string &normal, const std::string &touched, bool scaleEffect, bool brightnessEffect, bool isEnable)
+{
+    if (!this->TouchableSprite::initWithFiles(normal, normal, isEnable)) return false;
+    
+    _normalTexture  = Director::getInstance()->getTextureCache()->addImage(normal);
+    _touchedTexture = Director::getInstance()->getTextureCache()->addImage(touched);
+    
+    HRASSERT(_normalTexture,  "画像のファイルパスが不正です。: %s", normal.c_str());
+    HRASSERT(_touchedTexture, "画像のファイルパスが不正です。: %s", touched.c_str());
+    
+    scaleEffect      ? this->enableScaleEffect()      : disableScaleEffect();
+    brightnessEffect ? this->enableBrightnessEffect() : disableBrightnessEffect();
+    
+    return true;
+}
+
+Button *Button::createWithFile(const std::string &file,
+                               bool scaleEffect      /* = true */,
+                               bool brightnessEffect /* = true */,
+                               bool isEnable         /* = true */)
+{
+    return Button::createWithFiles(file, file, scaleEffect, brightnessEffect, isEnable);
+}
+
+
+
+#pragma mark - Control Methods
+
+void Button::enableScaleEffect()
+{
+    if (this->_touchingStatus == TouchingStatus::Touching || this->_touchingStatus == TouchingStatus::LongTouching)
+        this->switchScaleEffect(true);
+    
+    _isEnableScaleEffect = true;
+}
+
+void Button::disableScaleEffect()
+{
+    if (this->_touchingStatus == TouchingStatus::Touching || this->_touchingStatus == TouchingStatus::LongTouching)
+        this->switchScaleEffect(false);
+    
+    _isEnableScaleEffect = false;
+}
+
+void Button::enableBrightnessEffect()
+{
+    if (this->_touchingStatus == TouchingStatus::Touching || this->_touchingStatus == TouchingStatus::LongTouching)
+        this->switchBrightnessEffect(true);
+    
+    _isEnableBrightnessEffect = true;
+}
+
+void Button::disableBrightnessEffect()
+{
+    if (this->_touchingStatus == TouchingStatus::Touching || this->_touchingStatus == TouchingStatus::LongTouching)
+        this->switchBrightnessEffect(false);
+    
+    _isEnableBrightnessEffect = false;
+}
+
+
+
+#pragma mark - Touch Event Methods
+
+bool Button::onTouchBegan(cocos2d::Touch *touch, cocos2d::Event *event)
+{
+    if (!this->TouchableSprite::onTouchBegan(touch, event)) return false;
+    // ===== Call Super Method =====
+    
+    this->switchTouchEffect(true);
+    
+    return true;
+}
+
+void Button::onTouchMoved(cocos2d::Touch *touch, cocos2d::Event *event)
+{
+    this->TouchableSprite::onTouchMoved(touch, event);
+    // ===== Call Super Method =====
+    
+    this->switchTouchEffect(NodeUtility::isInsideNode(touch->getLocation(), this));
+}
+
+void Button::onTouchEnded(cocos2d::Touch *touch, cocos2d::Event *event)
+{
+    this->TouchableSprite::onTouchEnded(touch, event);
+    // ===== Call Super Method =====
+    
+    this->switchTouchEffect(false);
+}
+
+
+
+#pragma mark - Easing Methods
+
+void Button::switchScaleEffect(bool sw)
+{
+    if (!_isEnableScaleEffect) return;
+    
+    // setScale() だと元のスケールに戻せない為 runAction(ScaleTo) を使う
+    // 例: setScale(0.9) -> setScale(1.1) = 0.9 x 1.1 = 0.99
+    if (sw) this->runAction(ScaleTo::create(0.0f, 0.95f));
+    else    this->runAction(ScaleTo::create(0.0f, 1.0f));
+}
+
+void Button::switchBrightnessEffect(bool sw)
+{
+    if (!_isEnableBrightnessEffect) return;
+    
+    if (sw) this->setColor(Color3B::GRAY);
+    else    this->setColor(Color3B::WHITE);
+}
+
+void Button::switchTouchEffect(bool sw)
+{
+    this->switchScaleEffect(sw);
+    this->switchBrightnessEffect(sw);
+    
+    this->setTexture(sw ? _touchedTexture : _normalTexture);
+}
---
+++ UI/Button.h
@@ -0,0 +1,139 @@
+//
+//  Button.h
+//  HRStandardLibrary
+//
+//  Created by c1790 on 2015/11/16.
+//
+//
+
+/**
+ * Button クラス
+ *
+ * extend HR::TouchableSprite
+ *
+ * 一般的なボタン。
+ * 押下時に小さくしたり、暗くしたりする。on/off可
+ */
+
+#ifndef __HRStandardLibrary__Button__
+#define __HRStandardLibrary__Button__
+
+#include "TouchableSprite.h"
+
+
+namespace HR {
+
+
+class Button : public HR::TouchableSprite
+{
+CC_CONSTRUCTOR_ACCESS:
+    Button();
+    virtual ~Button();
+    
+    virtual bool init() override;
+    virtual bool initWithFiles(const std::string &normalFile,
+                               const std::string &touchedFile,
+                               bool scaleEffect,
+                               bool brightnessEffect,
+                               bool isEnable);
+//    virtual bool initWithFile(const std::string &file,
+//                              bool scaleEffect         = true,
+//                              bool brightnessEffect    = true,
+//                              bool isEnable            = true);
+    
+    
+public:
+#pragma mark - Create Methods
+    static Button *create();
+    /**
+     * @brief  ボタンを生成する
+     * @param normal           普段の画像ファイル
+     * @param touched          押下時の画像ファイル
+     * @param scaleEffect      (true)押下時に小さくするか
+     * @param brightnessEffect (true)押下時に暗くするか
+     * @param isEnable         (true)有効か
+     * @return ボタン
+     */
+    static Button *createWithFiles(const std::string &normalFile,
+                                   const std::string &touchedFile,
+                                   bool scaleEffect = true,
+                                   bool brightnessEffect = true,
+                                   bool isEnable = true);
+    /**
+     * @brief  ボタンを生成する
+     * @param  filePath         画像ファイル
+     * @param  scaleEffect      (true)押下時に小さくするか
+     * @param  brightnessEffect (true)押下時に暗くするか
+     * @param  isEnable         (true)有効か
+     * @return ボタン
+     */
+    static Button *createWithFile(const std::string &file,
+                                  bool scaleEffect = true,
+                                  bool brightnessEffect = true,
+                                  bool isEnable = true);
+
+    
+    
+#pragma mark - Control Methods
+    /**
+     * @brief 押下時のスケールエフェクト有効化する
+     */
+    void enableScaleEffect();
+    /**
+     * @brief 押下時のスケールエフェクトを無効化する
+     */
+    void disableScaleEffect();
+    
+    /**
+     * @brief 押下時の明暗エフェクトを有効化する
+     */
+    void enableBrightnessEffect();
+    /**
+     * @brief 押下時の明暗エフェクトを無効化する
+     */
+    void disableBrightnessEffect();
+    
+    
+#pragma mark - Members
+#pragma mark Read Only Members
+    CC_SYNTHESIZE_READONLY(cocos2d::Texture2D*, _normalTexture, NormalTexture);
+    CC_SYNTHESIZE_READONLY(cocos2d::Texture2D*, _touchedTexture, TouchedTexture);
+    CC_SYNTHESIZE_READONLY(bool, _isEnableScaleEffect, IsEnableScaleEffect);
+    CC_SYNTHESIZE_READONLY(bool, _isEnableBrightnessEffect, IsEnableBrightnessEffect);
+    
+    
+#pragma mark - Touch Event Methods
+protected:
+    virtual bool onTouchBegan(cocos2d::Touch *touch, cocos2d::Event *event) override;
+    virtual void onTouchMoved(cocos2d::Touch *touch, cocos2d::Event *event) override;
+    virtual void onTouchEnded(cocos2d::Touch *touch, cocos2d::Event *event) override;
+    
+    
+#pragma mark - Easing Methods
+private:
+    /**
+     * @brief スケールエフェクトをon/off
+     * @param sw on/off
+     */
+    void switchScaleEffect(bool sw);
+    
+    /**
+     * @brief 明暗エフェクトをon/off
+     * @param sw on/off
+     */
+    void switchBrightnessEffect(bool sw);
+    
+    /**
+     * @brief タッチエフェクト(2つ)をon/off
+     * @param sw on/off
+     */
+    void switchTouchEffect(bool sw);
+    
+    
+};
+
+
+}
+
+
+#endif /* defined(__HRStandardLibrary__Button__) */
