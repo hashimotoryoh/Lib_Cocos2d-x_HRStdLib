2ad5145d688b99891e2f82bd604fd439fe662fd6
hashimotoryoh <hashimotoryoh@gmail.com>
2016-02-22 20:10:58 +0900

    cocos2d::Valueに関するutilメソッドを色々追加

--- Helpers/ValueHelper/HRValueHelper.cpp
+++ Helpers/ValueHelper/HRValueHelper.cpp
@@ -14,13 +14,56 @@ using namespace HR;
 USING_NS_CC;
 
 
-bool HRValueHelper::isExistsKey(const cocos2d::ValueMap vm, const std::string &key)
+bool HRValueHelper::isEqual(const cocos2d::Value &value1, const cocos2d::Value &value2)
 {
-    return vm.find(key) != vm.end();
+    // 型が違えばもう違う
+    if (value1.getType() != value2.getType()) return false;
+    
+    /***** 以下、value1とvalue2の型が等しいことは保証される *****/
+    
+    switch (value1.getType()) {
+        case Value::Type::NONE:        return true;     // 中身が無いので比較する必要無し
+        case Value::Type::BYTE:        return (value1.asByte()   == value2.asByte());
+        case Value::Type::INTEGER:     return (value1.asInt()    == value2.asInt());
+        case Value::Type::FLOAT:       return (value1.asFloat()  == value2.asFloat());
+        case Value::Type::DOUBLE:      return (value1.asDouble() == value2.asDouble());
+        case Value::Type::BOOLEAN:     return (value1.asBool()   == value2.asBool());
+        case Value::Type::STRING:      return (value1.asString() == value2.asString());
+        case Value::Type::VECTOR:      return HRValueHelper::isEqualVector(value1.asValueVector(), value2.asValueVector());
+        case Value::Type::MAP:         return HRValueHelper::isEqualMap(value1.asValueMap(), value2.asValueMap());
+        case Value::Type::INT_KEY_MAP: return HRValueHelper::isEqualIntKeyMap(value1.asIntKeyMap(), value2.asIntKeyMap());
+    }
+    
+    HRERROR("cocos2d::Value型の値のタイプが不正です。: value1: %d, value2: %d", value1.getType(), value2.getType());
+    return false;
 }
 
 
-const std::string HRValueHelper::translateValueType(cocos2d::Value::Type type)
+bool HRValueHelper::isExistsKey(const cocos2d::ValueMap &map,
+                                const std::string &key)
+{
+    return map.find(key) != map.end();
+}
+
+bool HRValueHelper::isExistsKey(const cocos2d::ValueMapIntKey &map,
+                                const int key)
+{
+    return map.find(key) != map.end();
+}
+
+
+bool HRValueHelper::isContainsValue(const cocos2d::ValueVector &vector,
+                                    const cocos2d::Value value)
+{
+    for (Value comparison : vector)
+        // 同値が見つかった時点でtrueを返却
+        if (HRValueHelper::isEqual(value, comparison)) return true;
+    
+    return false;
+}
+
+
+const std::string HRValueHelper::translateValueType(const cocos2d::Value::Type type)
 {
     switch (type) {
         case Value::Type::NONE:        return "NONE";
@@ -37,4 +80,85 @@ const std::string HRValueHelper::translateValueType(cocos2d::Value::Type type)
     
     HRLOG("次に対応するValue::Typeは存在しません。: %d", type);
     return "ERROR";
+}
+
+
+
+#pragma mark - Private Methods
+
+bool HRValueHelper::isEqualVector(const cocos2d::ValueVector vector1,
+                                  const cocos2d::ValueVector vector2,
+                                  const bool considerOrder /* = false */)
+{
+    // 要素数が違えばもう違う
+    if (vector1.size() != vector2.size()) return false;
+    
+    /***** 以下、vector1とvector2の要素数が等しいことは保証される *****/
+    
+    if (considerOrder) {
+        // 順序を考慮する場合
+        // vector1, vector2の同じ要素番号に格納されている値が、全て同じなら同じ。
+        for (unsigned int i=0; i<vector1.size(); i++)
+            // 一つでも違えば違う。
+            if (!HRValueHelper::isEqual(vector1.at(i), vector2.at(i))) return false;
+    }
+    else {
+        // 順序を考慮しない場合
+        // 要素数が等しいことは保証されているので、value1の要素が全てvalue2に入っていれば同じ。
+        for (Value childValue1 : vector1)
+            // 一つでも違えば違う。
+            if (!HRValueHelper::isContainsValue(vector2, childValue1)) return false;
+    }
+    
+    return true;
+}
+
+
+bool HRValueHelper::isEqualMap(const cocos2d::ValueMap &map1,
+                               const cocos2d::ValueMap &map2)
+{
+    // 要素数が違えばもう違う
+    if (map1.size() != map2.size()) return false;
+    
+    /**** 以下、map1とmap2の要素数が等しいことは保証される *****/
+    
+    // 要素数が等しいことは保証されているので、value1の全ての要素について
+    //   - 同じキーが有る
+    //   - そのキーに対する値が等しい
+    // なら同じ。
+    for (std::pair<std::string, Value> pair : map1) {
+        const std::string key        = pair.first;
+        const Value       childValue = pair.second;
+        // 一つでも違えば違う。
+        if (!HRValueHelper::isExistsKey(map2, key)
+            ||
+            !HRValueHelper::isEqual(childValue, map2.at(key))) return false;
+    }
+    
+    return true;
+}
+
+
+bool HRValueHelper::isEqualIntKeyMap(const cocos2d::ValueMapIntKey &map1,
+                                     const cocos2d::ValueMapIntKey &map2)
+{
+    // 要素数が違えばもう違う
+    if (map1.size() != map2.size()) return false;
+    
+    /**** 以下、map1とmap2の要素数が等しいことは保証される *****/
+    
+    // 要素数が等しいことは保証されているので、value1の全ての要素について
+    //   - 同じキーが有る
+    //   - そのキーに対する値が等しい
+    // なら同じ。
+    for (std::pair<int, Value> pair : map1) {
+        const int key          = pair.first;
+        const Value childValue = pair.second;
+        // 一つでも違えば違う。
+        if (!HRValueHelper::isExistsKey(map2, key)
+            ||
+            !HRValueHelper::isEqual(childValue, map2.at(key))) return false;
+    }
+    
+    return true;
 }
--- Helpers/ValueHelper/HRValueHelper.h
+++ Helpers/ValueHelper/HRValueHelper.h
@@ -17,21 +17,126 @@ namespace HR {
 
 class HRValueHelper
 {
+#pragma mark - Static Methods
 public:
+#pragma mark Check Is Equal (cocos2d::Value) A and B
+    /**
+     * @brief  Valueの中身が等しいか調べる
+     * @param  value1 値1
+     * @param  value2 値2
+     * @return true/false 同/違
+     */
+    static bool isEqual(const cocos2d::Value &value1,
+                        const cocos2d::Value &value2);
+    
+    /**
+     * @brief  ValueVectorの中身が等しいか調べる
+     * @param  vector1       ベクター1
+     * @param  vector2       ベクター2
+     * @param  considerOrder (false)順序を考慮するか
+     * @return true/false 同/違
+     */
+    static bool isEqual(const cocos2d::ValueVector &vector1,
+                        const cocos2d::ValueVector &vector2,
+                        const bool considerOrder = false)
+    {
+        return HRValueHelper::isEqualVector(vector1, vector2, considerOrder);
+    };
+    
+    /**
+     * @brief  ValueMapの中身が全て等しいか調べる
+     * @param  map1 マップ1
+     * @param  map2 マップ2
+     * @return true/false 同/違
+     */
+    static bool isEqual(const cocos2d::ValueMap &map1,
+                        const cocos2d::ValueMap &map2)
+    {
+        return HRValueHelper::isEqualMap(map1, map2);
+    };
+    
+    /**
+     * @brief  ValueMapIntKeyの中身が全て等しいか調べる
+     * @param  map1 マップ1
+     * @param  map2 マップ2
+     * @return true/false 同/違
+     */
+    static bool isEqual(const cocos2d::ValueMapIntKey &map1,
+                        const cocos2d::ValueMapIntKey &map2)
+    {
+        return HRValueHelper::isEqualIntKeyMap(map1, map2);
+    };
+    
+    
+#pragma mark Check Is Exists Key In Map
     /**
      * @brief  ValueMapにキーが存在するか調べる
-     * @param  vm  対象のValueMap
-     * @param  key キー
+     * @param  map  対象のValueMap
+     * @param  key  キー
      * @return true/false
      */
-    static bool isExistsKey(const cocos2d::ValueMap vm, const std::string &key);
+    static bool isExistsKey(const cocos2d::ValueMap &map,
+                            const std::string &key);
     
+    /**
+     * @brief  ValueMapIntKeyにキーが存在するか調べる
+     * @param  map  対象のValueMapIntKey
+     * @param  key  キー
+     * @return true/false
+     */
+    static bool isExistsKey(const cocos2d::ValueMapIntKey &map,
+                            const int key);
+    
+    
+#pragma mark Check Is Contains Value In Vector
+    /**
+     * @brief  ValueVector中にValueが存在するか調べる
+     * @param  vector 対象のValueVector
+     * @param  value  値
+     * @return true/false 存在する/しない
+     */
+    static bool isContainsValue(const cocos2d::ValueVector &vector, const cocos2d::Value value);
+    
+    
+#pragma mark etc
     /**
      * @brief  Value::Typeを型名に変換する
      * @param  type
      * @return 型名
      */
-    static const std::string translateValueType(cocos2d::Value::Type type);
+    static const std::string translateValueType(const cocos2d::Value::Type type);
+    
+    
+#pragma mark - Private Methods
+private:
+    /**
+     * @brief  ValueVectorの中身が全て等しいか調べる
+     * @param  vector1       ベクター1
+     * @param  vector2       ベクター2
+     * @param  considerOrder (false)順序を考慮するか(する/しない)
+     * @return true/false 同/違
+     */
+    static bool isEqualVector(const cocos2d::ValueVector vector1,
+                              const cocos2d::ValueVector vector2,
+                              const bool considerOrder = false);
+    
+    /**
+     * @brief  ValueMapの中身が全て等しいか調べる
+     * @param  map1 マップ1
+     * @param  map2 マップ2
+     * @return true/false 同/違
+     */
+    static bool isEqualMap(const cocos2d::ValueMap &map1,
+                           const cocos2d::ValueMap &map2);
+    
+    /**
+     * @brief  ValueMapIntKeyの中身が全て等しいか調べる
+     * @param  map1 マップ1
+     * @param  map2 マップ2
+     * @return true/false 同/違
+     */
+    static bool isEqualIntKeyMap(const cocos2d::ValueMapIntKey &map1,
+                                 const cocos2d::ValueMapIntKey &map2);
     
     
 };
