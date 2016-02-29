ad3232ea6c93cbef4693f1228ed9eb5486602ef3
hashimotoryoh <hashimotoryoh@gmail.com>
2016-02-29 18:38:36 +0900

    0付きに対応 10c2195061a3aaca88e2e70e59ecc2245d6b310f の続き

--- Helpers/StringHelper/HRStringHelper.cpp
+++ Helpers/StringHelper/HRStringHelper.cpp
@@ -76,20 +76,20 @@ std::string HRStringHelper::getDate(std::string format,
     // フォーマットはPHPのdate()関数を踏襲
     std::string Y = HRStringHelper::intToString(1900 + time->tm_year);      // 西暦 4桁
     std::string y = HRStringHelper::intToString(time->tm_year % 100);       // 西暦 下2桁
-    std::string m = HRStringHelper::intToString(time->tm_mon + 1);          // 月 2桁 *****
+    std::string m = HRStringHelper::intToString(time->tm_mon + 1, 2);       // 月 2桁
     std::string n = HRStringHelper::intToString(time->tm_mon + 1);          // 月
     std::string F = MONTH_EN[time->tm_mon];                                 // 月 英語
     std::string M = F.substr(0, 3);                                         // 月 英語 3文字
-    std::string d = HRStringHelper::intToString(time->tm_mday);             // 日 2桁 *****
+    std::string d = HRStringHelper::intToString(time->tm_mday, 2);          // 日 2桁
     std::string j = HRStringHelper::intToString(time->tm_mday);             // 日
     std::string l = WEEKDAY_EN[time->tm_wday];                              // 曜日 英語
     std::string D = l.substr(0, 3);                                         // 曜日 英語 3文字
-    std::string H = HRStringHelper::intToString(time->tm_hour);             // 時 24時間表記 2桁 *****
+    std::string H = HRStringHelper::intToString(time->tm_hour, 2);          // 時 24時間表記 2桁
     std::string G = HRStringHelper::intToString(time->tm_hour);             // 時 24時間表記
-    std::string h = HRStringHelper::intToString((time->tm_hour <= 12) ? time->tm_hour : time->tm_hour-12);  // 時 12時間表記 2桁 *****
-    std::string g = HRStringHelper::intToString((time->tm_hour <= 12) ? time->tm_hour : time->tm_hour-12);  // 時 12時間表記
-    std::string i = HRStringHelper::intToString(time->tm_min);              // 分 2桁 *****
-    std::string s = HRStringHelper::intToString(time->tm_sec);              // 秒 2桁 *****
+    std::string h = HRStringHelper::intToString((time->tm_hour <= 12) ? time->tm_hour : time->tm_hour-12, 2);   // 時 12時間表記 2桁
+    std::string g = HRStringHelper::intToString((time->tm_hour <= 12) ? time->tm_hour : time->tm_hour-12);      // 時 12時間表記
+    std::string i = HRStringHelper::intToString(time->tm_min, 2);           // 分 2桁
+    std::string s = HRStringHelper::intToString(time->tm_sec, 2);           // 秒 2桁
     std::string a = (time->tm_hour <= 12) ? "am" : "pm";                    // 午前/午後 英語 小文字
     std::string A = (time->tm_hour <= 12) ? "AM" : "PM";                    // 午前/午後 英語 大文字
     
